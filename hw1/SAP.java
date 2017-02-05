155c7e87ed7d5a10b468ecb0aa89274f69025121
Rastislav Ondrasek <ras.ondrasek@gmail.com>
2017-02-05 14:31:02 -0800

    [hw1] Reindenting code

--- hw1/SAP.java
+++ hw1/SAP.java
@@ -1,140 +1,144 @@
-import java.util.*;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Optional;
 
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
 public class SAP {
 
-   private Digraph G;
+    private Digraph G;
 
-   // constructor takes a digraph (not necessarily a DAG)
-   public SAP(Digraph G) {
-      this.G = G;
-   }
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        this.G = G;
+    }
 
-   private Map<Integer, Integer> bfs(int s) {
-      Map<Integer, Integer> distancesFromS = new HashMap<>();
-      distancesFromS.put(s, 0);
-      Deque<Integer> queue = new LinkedList<>();
-      queue.add(s);
+    private Map<Integer, Integer> bfs(int s) {
+        Map<Integer, Integer> distancesFromS = new HashMap<>();
+        distancesFromS.put(s, 0);
+        Deque<Integer> queue = new LinkedList<>();
+        queue.add(s);
 
-      while (!queue.isEmpty()) {
-         int v = queue.remove();
-         int distanceToV = distancesFromS.get(v);
-         for (int adjacentToV : G.adj(v)) {
-            if (!distancesFromS.containsKey(adjacentToV)) {  // TODO: We can optimize by reducing autoboxing
-               queue.add(adjacentToV);
-               distancesFromS.put(adjacentToV, distanceToV + 1);
+        while (!queue.isEmpty()) {
+            int v = queue.remove();
+            int distanceToV = distancesFromS.get(v);
+            for (int adjacentToV : G.adj(v)) {
+                if (!distancesFromS.containsKey(adjacentToV)) {  // TODO: We can optimize by reducing autoboxing
+                    queue.add(adjacentToV);
+                    distancesFromS.put(adjacentToV, distanceToV + 1);
+                }
             }
-         }
-      }
+        }
 
-      return distancesFromS;
-   }
+        return distancesFromS;
+    }
 
-   private static class NearestAncestor {
-      int ancestor;
-      int distance;
+    private static class NearestAncestor {
+        int ancestor;
+        int distance;
 
-      public NearestAncestor(int ancestor, int distance) {
-         this.ancestor = ancestor;
-         this.distance = distance;
-      }
-   }
+        public NearestAncestor(int ancestor, int distance) {
+            this.ancestor = ancestor;
+            this.distance = distance;
+        }
+    }
 
-   private Optional<NearestAncestor> getNearestAncestor(int v, int w) {
-      Optional<NearestAncestor> nearestAncestor = Optional.empty();
-      Map<Integer, Integer> distancesFromV = bfs(v);
-      Map<Integer, Integer> distancesFromW = bfs(w);
-      int ancestor = -1;
-      int minDistance = Integer.MAX_VALUE;
+    private Optional<NearestAncestor> getNearestAncestor(int v, int w) {
+        Optional<NearestAncestor> nearestAncestor = Optional.empty();
+        Map<Integer, Integer> distancesFromV = bfs(v);
+        Map<Integer, Integer> distancesFromW = bfs(w);
+        int ancestor = -1;
+        int minDistance = Integer.MAX_VALUE;
 
-      for (Map.Entry<Integer, Integer> distanceFromV : distancesFromV.entrySet()) {
-         Integer x = distanceFromV.getKey();
-         if (distancesFromW.containsKey(x)) {
-            if (minDistance > distancesFromV.get(x) + distancesFromW.get(x)) {
-               ancestor = x;
-               minDistance = distancesFromV.get(x) + distancesFromW.get(x);
+        for (Map.Entry<Integer, Integer> distanceFromV : distancesFromV.entrySet()) {
+            Integer x = distanceFromV.getKey();
+            if (distancesFromW.containsKey(x)) {
+                if (minDistance > distancesFromV.get(x) + distancesFromW.get(x)) {
+                    ancestor = x;
+                    minDistance = distancesFromV.get(x) + distancesFromW.get(x);
+                }
             }
-         }
-      }
+        }
 
-      if (ancestor != -1) {
-         nearestAncestor = Optional.of(new NearestAncestor(ancestor, minDistance));
-      }
+        if (ancestor != -1) {
+            nearestAncestor = Optional.of(new NearestAncestor(ancestor, minDistance));
+        }
 
-      return nearestAncestor;
-   }
+        return nearestAncestor;
+    }
 
-   // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w) {
-      int distance = -1;
-      Optional<NearestAncestor> nearestAncestor = getNearestAncestor(v, w);
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        int distance = -1;
+        Optional<NearestAncestor> nearestAncestor = getNearestAncestor(v, w);
 
-      if (nearestAncestor.isPresent()) {
-         distance = nearestAncestor.get().distance;
-      }
+        if (nearestAncestor.isPresent()) {
+            distance = nearestAncestor.get().distance;
+        }
 
-      return distance;
-   }
+        return distance;
+    }
 
-   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-   public int ancestor(int v, int w) {
-      int ancestor = -1;
-      Optional<NearestAncestor> nearestAncestor = getNearestAncestor(v, w);
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        int ancestor = -1;
+        Optional<NearestAncestor> nearestAncestor = getNearestAncestor(v, w);
 
-      if (nearestAncestor.isPresent()) {
-         ancestor = nearestAncestor.get().ancestor;
-      }
+        if (nearestAncestor.isPresent()) {
+            ancestor = nearestAncestor.get().ancestor;
+        }
 
-      return ancestor;
-   }
+        return ancestor;
+    }
 
-   // length of shortest ancestral path between any v in v and any v in w; -1 if no such path
-   public int length(Iterable<Integer> vs, Iterable<Integer> ws) {
-      int minDistance = Integer.MAX_VALUE;
+    // length of shortest ancestral path between any v in v and any v in w; -1 if no such path
+    public int length(Iterable<Integer> vs, Iterable<Integer> ws) {
+        int minDistance = Integer.MAX_VALUE;
 
-      for (Integer v : vs) {
-         for (Integer w : ws) {
-            int distance = length(v, w);
-            if (distance != -1 && distance < minDistance) {
-               minDistance = distance;
+        for (Integer v : vs) {
+            for (Integer w : ws) {
+                int distance = length(v, w);
+                if (distance != -1 && distance < minDistance) {
+                    minDistance = distance;
+                }
             }
-         }
-      }
+        }
 
-      if (minDistance == Integer.MAX_VALUE) {
-         minDistance = -1;
-      }
+        if (minDistance == Integer.MAX_VALUE) {
+            minDistance = -1;
+        }
 
-      return minDistance;
-   }
+        return minDistance;
+    }
 
-   // a common ancestor that participates in shortest ancestral path; -1 if no such path
-   public int ancestor(Iterable<Integer> vs, Iterable<Integer> ws) {
-      int minAncestor = -1;
-      int minDistance = Integer.MAX_VALUE;
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> vs, Iterable<Integer> ws) {
+        int minAncestor = -1;
+        int minDistance = Integer.MAX_VALUE;
 
-      for (Integer v : vs) {
-         for (Integer w : ws) {
-            Optional<NearestAncestor> nearestAncestor = getNearestAncestor(v, w);
-            if (nearestAncestor.isPresent() && nearestAncestor.get().distance < minDistance) {
-               minAncestor = nearestAncestor.get().ancestor;
-               minDistance = nearestAncestor.get().distance;
+        for (Integer v : vs) {
+            for (Integer w : ws) {
+                Optional<NearestAncestor> nearestAncestor = getNearestAncestor(v, w);
+                if (nearestAncestor.isPresent() && nearestAncestor.get().distance < minDistance) {
+                    minAncestor = nearestAncestor.get().ancestor;
+                    minDistance = nearestAncestor.get().distance;
+                }
             }
-         }
-      }
+        }
 
-      return minAncestor;
-   }
+        return minAncestor;
+    }
 
-   // do unit testing of this class
-   public static void main(String[] args) {
-      Digraph G = new Digraph(new In(args[0]));
-      SAP sap = new SAP(G);
-      System.out.println("length = " + sap.length(3, 11) + ", ancestor = " + sap.ancestor(3, 11));
-      System.out.println("length = " + sap.length(9, 12) + ", ancestor = " + sap.ancestor(9, 12));
-      System.out.println("length = " + sap.length(7, 2) + ", ancestor = " + sap.ancestor(7, 2));
-      System.out.println("length = " + sap.length(1, 6) + ", ancestor = " + sap.ancestor(1, 6));
-   }
+    // do unit testing of this class
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+        SAP sap = new SAP(G);
+        System.out.println("length = " + sap.length(3, 11) + ", ancestor = " + sap.ancestor(3, 11));
+        System.out.println("length = " + sap.length(9, 12) + ", ancestor = " + sap.ancestor(9, 12));
+        System.out.println("length = " + sap.length(7, 2) + ", ancestor = " + sap.ancestor(7, 2));
+        System.out.println("length = " + sap.length(1, 6) + ", ancestor = " + sap.ancestor(1, 6));
+    }
 }
--- hw1/WordNet.java
+++ hw1/WordNet.java
@@ -7,81 +7,81 @@ import edu.princeton.cs.algs4.In;
 
 public class WordNet {
 
-   private Map<String, Integer> nounToSynsetIdMap;
-   private Map<Integer, String> synsetIdToSynsetMap;
-   private SAP sap;
+    private Map<String, Integer> nounToSynsetIdMap;
+    private Map<Integer, String> synsetIdToSynsetMap;
+    private SAP sap;
 
-   // constructor takes the name of the two input files
-   public WordNet(String synsets, String hypernyms) {
-      this.nounToSynsetIdMap = new HashMap<>();
-      this.synsetIdToSynsetMap = new HashMap<>();
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+        this.nounToSynsetIdMap = new HashMap<>();
+        this.synsetIdToSynsetMap = new HashMap<>();
 
-      In in = new In(synsets);
-      while (in.hasNextLine()) {
-         String[] tokens = in.readLine().split(",");
-         Integer synsetId = Integer.parseInt(tokens[0]);
-         String synset = tokens[1];
-         synsetIdToSynsetMap.put(synsetId, synset);
+        In in = new In(synsets);
+        while (in.hasNextLine()) {
+            String[] tokens = in.readLine().split(",");
+            Integer synsetId = Integer.parseInt(tokens[0]);
+            String synset = tokens[1];
+            synsetIdToSynsetMap.put(synsetId, synset);
 
-         String[] nouns = synset.split(" ");
-         for (int i = 0; i < nouns.length; ++i) {
-            nounToSynsetIdMap.put(nouns[i], synsetId);
-         }
-      }
+            String[] nouns = synset.split(" ");
+            for (int i = 0; i < nouns.length; ++i) {
+                nounToSynsetIdMap.put(nouns[i], synsetId);
+            }
+        }
 
-      Digraph G = new Digraph(synsetIdToSynsetMap.size());
-      in = new In(hypernyms);
-      while (in.hasNextLine()) {
-         String[] tokens = in.readLine().split(",");
-         int v = Integer.parseInt(tokens[0]);
-         for (int i = 1; i < tokens.length; ++i) {
-            G.addEdge(v, Integer.parseInt(tokens[i]));
-         }
-      }
+        Digraph G = new Digraph(synsetIdToSynsetMap.size());
+        in = new In(hypernyms);
+        while (in.hasNextLine()) {
+            String[] tokens = in.readLine().split(",");
+            int v = Integer.parseInt(tokens[0]);
+            for (int i = 1; i < tokens.length; ++i) {
+                G.addEdge(v, Integer.parseInt(tokens[i]));
+            }
+        }
 
-      this.sap = new SAP(G);
-   }
+        this.sap = new SAP(G);
+    }
 
-   // returns all WordNet nouns
-   public Iterable<String> nouns() {
-      return nounToSynsetIdMap.keySet();
-   }
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        return nounToSynsetIdMap.keySet();
+    }
 
-   // is the word a WordNet noun?
-   public boolean isNoun(String word) {
-      return nounToSynsetIdMap.containsKey(word);
-   }
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        return nounToSynsetIdMap.containsKey(word);
+    }
 
-   // distance between nounA and nounB (defined below)
-   public int distance(String nounA, String nounB) {
-      if (!isNoun(nounA) || !isNoun(nounB)) {
-         throw new IllegalArgumentException();
-      }
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException();
+        }
 
-      return sap.length(nounToSynsetIdMap.get(nounA), nounToSynsetIdMap.get(nounB));
-   }
+        return sap.length(nounToSynsetIdMap.get(nounA), nounToSynsetIdMap.get(nounB));
+    }
 
-   // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-   // in a shortest ancestral path (defined below)
-   public String sap(String nounA, String nounB) {
-      if (!isNoun(nounA) || !isNoun(nounB)) {
-         throw new IllegalArgumentException();
-      }
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException();
+        }
 
-      String synset = "";
-      int ancestor = sap.ancestor(nounToSynsetIdMap.get(nounA), nounToSynsetIdMap.get(nounB));
-      if (ancestor != -1) {
-         synset = synsetIdToSynsetMap.get(ancestor);
-      }
+        String synset = "";
+        int ancestor = sap.ancestor(nounToSynsetIdMap.get(nounA), nounToSynsetIdMap.get(nounB));
+        if (ancestor != -1) {
+            synset = synsetIdToSynsetMap.get(ancestor);
+        }
 
-      return synset;
-   }
+        return synset;
+    }
 
-   // do unit testing of this class
-   public static void main(String[] args) {
-      WordNet wn = new WordNet(args[0], args[1]);
-      System.out.println(wn.sap("Actifed", "Coricidin"));  // Should return "antihistamine"
-      System.out.println(wn.sap("Aegisthus", "antihistamine"));  // Should return ""
-   }
+    // do unit testing of this class
+    public static void main(String[] args) {
+        WordNet wn = new WordNet(args[0], args[1]);
+        System.out.println(wn.sap("Actifed", "Coricidin"));  // Should return "antihistamine"
+        System.out.println(wn.sap("Aegisthus", "antihistamine"));  // Should return ""
+    }
 }
 
