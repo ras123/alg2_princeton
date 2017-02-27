b351e1ef70730e15c2559571e7d2f006c7c35d8f
Rastislav Ondrasek <ras.ondrasek@gmail.com>
2017-02-26 21:16:12 -0800

    [hw3] Parsing input and building team data structure

--- hw3/BaseballElimination.java
+++ hw3/BaseballElimination.java
@@ -1,34 +1,103 @@
-public class BaseballElimination {
-    public BaseballElimination(String filename) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BaseballElimination {
+
+    private Map<String, Team> teams;
+
+    private static class Team {
+        private String name;
+        private int wins;
+        private int losses;
+        private int remaining;
+        private Map<String, Integer> remainingAgainst;
+
+        public Team(String name, int wins, int losses, int remaining) {
+            this.name = name;
+            this.wins = wins;
+            this.losses = losses;
+            this.remaining = remaining;
+            this.remainingAgainst = new HashMap<>();
+        }
+
+        public int wins() {
+            return wins;
+        }
+
+        public int losses() {
+            return losses;
+        }
+
+        public int remaining() {
+            return remaining;
+        }
+
+        public void setRemainingAgainst(String team, int games) {
+            remainingAgainst.put(team, games);
+        }
+
+        public int getRemainingAgainst(String team) {
+            return remainingAgainst.get(team);
+        }
+    }
+
+    public BaseballElimination(String filename) {
+        this.teams = new HashMap<>();
+
+        In in = new In(filename);
+        String[] teamNames = new String[in.readInt()];
+        String[] remainingAgainst = new String[teamNames.length];
+        int idx = 0;
+
+        while (!in.isEmpty()) {
+            teamNames[idx] = in.readString();
+            Team team = new Team(teamNames[idx], in.readInt(), in.readInt(), in.readInt());
+            teams.put(teamNames[idx], team);
+            remainingAgainst[idx] = in.readLine();
+            ++idx;
+        }
+
+        for (int i = 0; i < remainingAgainst.length; ++i) {
+            String teamName = teamNames[i];
+            StringTokenizer tokenizer = new StringTokenizer(remainingAgainst[i]);
+            for (int j = 0; j < teamNames.length; ++j) {
+                String otherTeamName = teamNames[j];
+                int games = Integer.parseInt(tokenizer.nextToken());
+                teams.get(teamName).setRemainingAgainst(otherTeamName, games);
+            }
+        }
     }
 
     public int numberOfTeams() {
-        return -1;
+        return teams.size();
     }
 
     public Iterable<String> teams() {
-        return null;
+        return teams.keySet();
     }
 
     // Number of wins for given team
     public int wins(String team) {
-        return -1;
+        return teams.get(team).wins();
     }
 
     // Number of losses for given team
     public int losses(String team) {
-        return -1;
+        return teams.get(team).losses();
     }
 
     // Number of remaining games for given team
     public int remaining(String team) {
-        return -1;
+        return teams.get(team).remaining();
     }
 
     // Number of remaining games between team1 and team2
     public int against(String team1, String team2) {
-        return -1;
+        return teams.get(team1).getRemainingAgainst(team2);
     }
 
     public boolean isEliminated(String team) {
@@ -38,5 +107,21 @@ public class BaseballElimination {
     // Subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
     }
 }
